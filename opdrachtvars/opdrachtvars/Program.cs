2afba864912c6202cefdfa40163e7725d39901f7
Bryan-van-Riessen-theBvR <75975096+Bryan-van-Riessen-theBvR@users.noreply.github.com>
2021-05-21 15:21:21 +0200

    Add files via upload

---
+++ opdrachtvars/opdrachtvars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace opdrachtvars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Greetings " + userName);
+        }
+    }
+}
